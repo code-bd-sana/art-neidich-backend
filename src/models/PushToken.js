f416d54676fcf0d6dcccff23fe84221b61262fe9
JoySarkarBD <developer.joysarkar@gmail.com>
2026-02-02 09:41:54 +0600

    refactor: remove default values for loggedInStatus and timestamps in PushToken model

--- src/models/PushToken.js
+++ src/models/PushToken.js
@@ -19,15 +19,12 @@ const pushTokenSchema = new mongoose.Schema(
         }, // toggle notifications per user
         loggedInStatus: {
           type: Boolean,
-          default: true,
         }, // track if user is logged in on this device
         lastLoggedInAt: {
           type: Date,
-          default: null,
         }, // timestamp of last login status update
         lastLoggedOutAt: {
           type: Date,
-          default: null,
         }, // timestamp of last logout status update
       },
     ],
